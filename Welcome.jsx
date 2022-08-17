9f6ebef6deed12009967985d863b1e517dd04f0e
HarishKamuni <108679987+HarishKamuni@users.noreply.github.com>
2022-08-17 12:52:53 +0530

    test

---
+++ Welcome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Welcome(){
+    const name = "Harish";
+    return(
+        <>
+            <h1>Hey! {name}</h1>
+            <h2>Welcome to Newton School.</h2>
+        </>
+    );
+    
+}
+export default Welcome;
